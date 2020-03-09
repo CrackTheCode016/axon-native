f1d6c54a9dcee75421444795df7932b1b234932c
Bader <bader@Baders-MacBook-Pro-615.local>
2020-03-09 16:54:59 -0400

    final refactor
    identity
    
    identity stuff

--- src/handshake.rs
+++ src/handshake.rs
@@ -3,26 +3,29 @@ pub mod handshake {
     use crate::serial::serial_handler::SerialData;
     use serde::de::DeserializeOwned;
     use serde::{Deserialize, Serialize};
+    use serde_repr::*;
     use serialport::prelude::*;
     use std::borrow::BorrowMut;
     use std::io::{Error, ErrorKind};
 
     const HANDSHAKE_PREFIX: char = 'H';
 
-    #[derive(Deserialize, Serialize, Debug, PartialEq)]
+    #[derive(Serialize_repr, Deserialize_repr, Debug, PartialEq)]
+    #[repr(i16)]
     pub enum AxonHandshakeType {
         HandshakeConnect = 18499,
         HandshakeAccept = 18497,
     }
 
-    #[derive(Serialize, Deserialize, PartialEq)]
+    #[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug)]
+    #[repr(i8)]
     pub enum AxonMessageType {
         RecordMessage = 0,
         StateMessage = 1,
         CommandMessage = 2,
     }
 
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, Debug)]
     #[serde(rename_all = "camelCase")]
     pub struct HandshakeRequest {
         handshake_type: AxonHandshakeType,
@@ -64,34 +67,32 @@ pub mod handshake {
             let accept = HandshakeResponse {
                 handshake_type: AxonHandshakeType::HandshakeAccept,
             };
+            println!("{:?}", message_type);
             let data: String = SerialData::read_port(port.borrow_mut())?;
+
+            let mut accept_stringifed = String::new();
+            accept_stringifed.push(HANDSHAKE_PREFIX);
+            accept_stringifed.push_str(&accept.to_json_string()?);
             if Self::check_type_from_str::<HandshakeRequest>(&data) {
                 let parsed: Result<HandshakeRequest, serde_json::Error> =
                     serde_json::from_str(&data);
-
-                let mut accept_stringifed = String::new();
-                accept_stringifed.push(HANDSHAKE_PREFIX);
-                accept_stringifed.push_str(&accept.to_json_string()?);
-
+                    println!("RECIEVED SOME REQUEST {:?}", parsed);
                 match parsed {
                     Ok(result) => match result.handshake_type {
                         AxonHandshakeType::HandshakeConnect => {
+                            println!("{}, {:?}, {:?}", result.message_type == message_type, result.message_type, message_type);
                             if result.message_type == message_type {
                                 SerialData::write_port(accept_stringifed, port.borrow_mut())?;
                                 Ok(loop {
-                                    // todo: maybe add a limit, as the loop can go on forever
                                     let data = SerialData::read_port(port.borrow_mut())?;
+                                    println!("Now we wait {}", data);
                                     if Self::check_type_from_str::<T>(&data) {
                                         let message: Message<T> = serde_json::from_str(&data)?;
-                                        port.flush()?;
                                         break message;
                                     }
                                 })
                             } else {
-                                Ok(Message::Log {
-                                    status: 0,
-                                    data: data,
-                                })
+                                Ok(Message::Empty)
                             }
                         }
                         _ => Ok(Message::Empty),
@@ -116,14 +117,20 @@ pub mod handshake {
                 handshake_type: AxonHandshakeType::HandshakeConnect,
                 message_type: message_type,
             };
-            SerialData::write_port(connect.to_json_string()?, port.borrow_mut())?;
+            let mut connect_stringifed = String::new();
+            connect_stringifed.push(HANDSHAKE_PREFIX);
+            connect_stringifed.push_str(&connect.to_json_string()?);
+            SerialData::write_port(connect_stringifed, port.borrow_mut())?;
             Ok(loop {
                 let data = SerialData::read_port(port.borrow_mut())?;
                 let result: Result<HandshakeResponse, ()> = serde_json::from_str(&data)?;
                 match result {
                     Ok(response) => match response.handshake_type {
                         AxonHandshakeType::HandshakeAccept => {
-                            SerialData::write_port(sendable.to_json_string()?, port.borrow_mut())?;
+                            let mut sendable_stringified = String::new();
+                            sendable_stringified.push(prefix_byte);
+                            sendable_stringified.push_str(&sendable.to_json_string()?);
+                            SerialData::write_port(sendable_stringified, port.borrow_mut())?;
                             break AxonMessageStatus::Success;
                         }
                         _ => (),
